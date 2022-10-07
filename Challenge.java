5da8d4fb7be130349f59ac833f4bcc46e1bfcba1
solobolo1 <72794428+solobolo1@users.noreply.github.com>
2022-10-07 13:50:16 +0530

    Update Challenge.java

--- Challenge.java
+++ Challenge.java
@@ -1,22 +1,30 @@
 public class Challenge {
     public static void main(String[] args) {
-        System.out.println("P:" + sumDigProd(1, 2, 3, 4, 1,7,3,8,1,4,6,234));
+        System.out.println("Final:" + sumDigProd(6,6,6,6));
     }
 
     public static double sumDigProd(int... args) {
         int s = 0;
         double prod = 1;
+
         for (int arg : args) {
             s += arg;
         }
-        System.out.println(s);
-        for (int i=1; i<=String.valueOf(s).length(); i++) {
-            double val = Math.pow(10, i);
-            if (val >= 10) {
-                s /= Math.pow(10, i);
+        System.out.println("ini sum:" + s);
+        if (s > 0) {
+            for (int i=1; i<=String.valueOf(s).length(); i++) {
+                double val = Math.pow(10, 1);
+                System.out.println("val:" + val);
+                prod *= (s%val);
+
+                s /= 10;
+                System.out.println("s after div:" + s);
+                s = (int) s;
+                System.out.println("s no dec:" +s);
+                System.out.println("product:" + prod);
+
             }
-            System.out.println(val);
-            prod *= (s%val);
+            return prod;
         }
         return prod;
     }
